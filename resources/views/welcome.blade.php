3a44ff597bcd1a73c94cde925e7de282e148787f
Nick <nickquispel@outlook.com>
2020-12-29 10:50:52 +0000

    adjusted website information

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,7 +31,6 @@
                     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
                     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
                     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
                     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{ url('login') }}">Login</a></li>
                 </ul>
             </div>
@@ -41,7 +40,7 @@
             <!-- About-->
             <section class="resume-section" id="about">
                 <div class="resume-section-content">
-                    <h1 class="mb-0">php
+                    <h1 class="mb-0">
                         Nick
                         <span class="text-primary">Quispel</span>
                     </h1>
@@ -66,7 +65,7 @@
                         <div class="flex-grow-1">
                             <h3 class="mb-0">Junior backend Developer</h3>
                             <div class="subheading mb-3">Jorr-IT Solutions</div>
-                            <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+                            <p>Developing customized Oracle apex applications for data integrations between different applications (e.g. Topdesk, Freshdesk, Exact Online, Afas, 2BA, Cloudsuite, Routit). Programming langauges: PLSQL, Javacript (nodejs), Java (Springboot), Python. </p>
                         </div>
                         <div class="flex-shrink-0"><span class="text-primary">September 2019 - Present</span></div>
                     </div>
@@ -74,7 +73,7 @@
                         <div class="flex-grow-1">
                             <h3 class="mb-0">Internship Developer</h3>
                             <div class="subheading mb-3">Xsarus</div>
-                            <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+                            <p>Learning how to build webshops with Magento2. Programming languages: PHP,Javascript,HTML,CSS.</p>
                         </div>
                         <div class="flex-shrink-0"><span class="text-primary">August 2019 - December 2019</span></div>
                     </div>
@@ -82,7 +81,7 @@
                         <div class="flex-grow-1">
                             <h3 class="mb-0">Account manager</h3>
                             <div class="subheading mb-3">Gasmeetstation</div>
-                            <p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>
+                            <p>Creating the most optimal customer serice for several activities (Gas measurements, transport, customs inspections). </p>
                         </div>
                         <div class="flex-shrink-0"><span class="text-primary">November 2018 - July 2019</span></div>
                     </div>
@@ -90,7 +89,7 @@
                         <div class="flex-grow-1">
                             <h3 class="mb-0">Account manager</h3>
                             <div class="subheading mb-3">Neele-Vat Logistics</div>
-                            <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+                            <p>Creating the most optimal customer serice for several activities (customs clearence, customs inspections, transport, Dangerous goods regulations). </p>
                         </div>
                         <div class="flex-shrink-0"><span class="text-primary">January 2015 - August 2017</span></div>
                     </div>
@@ -145,7 +144,7 @@
                         <li class="list-inline-item"><i class="fab fa-java"></i></li>
                         
                     </ul>
-                    <div class="subheading mb-3">Workflow</div>
+                    <!-- <div class="subheading mb-3">Workflow</div>
                     <ul class="fa-ul mb-0">
                         <li>
                             <span class="fa-li"><i class="fas fa-check"></i></span>
@@ -164,63 +163,18 @@
                             Agile Development & Scrum
                         </li>
                     </ul>
-                </div>
+                </div> -->
             </section>
             <hr class="m-0" />
             <!-- Interests-->
             <section class="resume-section" id="interests">
                 <div class="resume-section-content">
                     <h2 class="mb-5">Interests</h2>
-                    <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
-                    <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+                    <p>Apart from being a web developer, I enjoy most of my time being outdoors. I'm a big fan of mountainbiking and going to the gym and hiking. On the other hand I can also spend my time inside with gaming or developing.</p>
                 </div>
             </section>
             <hr class="m-0" />
             <!-- Awards-->
-            <section class="resume-section" id="awards">
-                <div class="resume-section-content">
-                    <h2 class="mb-5">Awards & Certifications</h2>
-                    <ul class="fa-ul mb-0">
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            Google Analytics Certified Developer
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            Mobile Web Specialist - Google Certification
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - University of Colorado Boulder - Emerging Tech Competition 2009
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            2
-                            <sup>nd</sup>
-                            Place - University of Colorado Boulder - Emerging Tech Competition 2008
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - James Buchanan High School - Hackathon 2006
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            3
-                            <sup>rd</sup>
-                            Place - James Buchanan High School - Hackathon 2005
-                        </li>
-                    </ul>
-                </div>
             </section>
         </div>
         <!-- Bootstrap core JS-->
